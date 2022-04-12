999de8b315c93f17b7bba19299fa004432559cd3
marcocarioti <103490350+marcocarioti@users.noreply.github.com>
2022-04-12 10:08:11 +0200

    Esercizio_login

---
+++ codice.js
@@ -0,0 +1,11 @@
+
+function miaFunzione() {
+document.getElementById("usr").value = "";
+document.getElementById("pass").value = "";
+}
+
+
+function loginMio() {
+document.getElementById("lognBtnDiv").innerHTML = "Login Effettuato";
+
+}
